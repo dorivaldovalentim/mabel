33af0e7e03b6fed4b948b3dff6b0b02001a66be2
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-09-10 08:38:52 +0100

    Adição dos primeiros assets (controller, routes) para a camada de admin

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
+    Route::get('/', 'AdminController@index')->name('admin');
+    Route::get('/home', 'AdminController@index')->name('admin');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
