e09cdb76f26eec53d9d0666f211aeab287dac9e5
Arão Domingos <araodomingos04@gmail.com>
2021-09-18 21:49:14 -0300

    concluido a view de teste para show/create de porifolios

--- resources/views/admin/portifolio/edit.blade.php
+++ resources/views/admin/portifolio/edit.blade.php
@@ -1,37 +1,29 @@
 @extends('layouts.app')
-@section('title', 'Lista de Clientes')
 
 @section('content')
-    <div class="card container col-md-8">
-        <table class="table table-hover text-center">
-            <thead>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Operações</th>
-            </thead>
-            <tbody>
-                @foreach ($portifolios as $portifolio)
-                    
-                    <tr>
-                        <td>{{ $portifolio->id }}</td>
-                        <td>{{ $portifolio->name }}</td>
-                        <td class="row justify-content-center">
-                            <a href="{{ route('portifolio.show', ['portifolio' => $portifolio->id]) }}" class="mr-2">
-                                <button class="btn btn-primary">Ver</button>
-                            </a>
-                            <a href="{{ route('portifolio.edit', ['portifolio' => $portifolio->id]) }}" class="mr-2">
-                                <button class="btn btn-success">Editar</button>
-                            </a>
-                            <form method="POST" action="{{ route('portifolio.destroy', ['portifolio' => $portifolio->id]) }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Remover</button>
-                            </form>
-                        </td>
-                    </tr>
-
-                @endforeach
-            </tbody>
-        </table>
+    <div class="container col-md-8 col-lg-">
+            <form action="{{ route('portifolio.update', ['portifolio' => $portifolio->id ]) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label>Nome</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Descrição</label>
+                    <textarea name="description" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label>File</label>
+                    <input type="file" name="file" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Tipo de Ficheiro</label>
+                    <select name="typeFile" class="form-control">
+                        <option>Video</option>
+                        <option>Imagem</option>
+                    </select>
+                </div>
+                <input type="submit" value="cadastrar" class="btn btn-primary">
+            </form>
     </div>
 @endsection
