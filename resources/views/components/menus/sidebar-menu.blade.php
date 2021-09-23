06972d1497d0d99d0d7f97b0dfd71fbfd539e9d7
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-09-23 11:41:46 +0100

    Tornando algumas seções do template dinâmicas

--- resources/views/components/menus/sidebar-menu.blade.php
+++ resources/views/components/menus/sidebar-menu.blade.php
@@ -1,145 +1,72 @@
 <aside class="mdc-drawer mdc-drawer--dismissible mdc-drawer--open">
     <div class="mdc-drawer__header">
-        <a href="index.html" class="brand-logo">
+        <a href="{{ route('admin') }}" class="brand-logo">
             <img src="{{ asset('admin/images/logo_red_white_without_legend.png') }}" width="100%" alt="logo">
         </a>
     </div>
     <div class="mdc-drawer__content">
         <div class="user-info">
-            <p class="name">Clyde Miles</p>
-            <p class="email">clydemiles@elenor.us</p>
+            <p class="name">{{ auth()->user()->name }}</p>
+            <p class="email">{{ auth()->user()->email }}</p>
         </div>
         <div class="mdc-list-group">
             <nav class="mdc-list mdc-drawer-menu">
                 <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="index.html">
+                    <a class="mdc-drawer-link" href="{{ route('admin') }}">
                         <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
                             aria-hidden="true">home</i>
-                        Dashboard
+                        Início
                     </a>
                 </div>
                 <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="pages/forms/basic-forms.html">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">track_changes</i>
-                        Forms
-                    </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="ui-sub-menu">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">dashboard</i>
-                        UI Features
+                    <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="portfolios-sub-menu">
+                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">dashboard</i>
+                        Portfólios
                         <i class="mdc-drawer-arrow material-icons">chevron_right</i>
                     </a>
-                    <div class="mdc-expansion-panel" id="ui-sub-menu">
+
+                    <div class="mdc-expansion-panel" id="portfolios-sub-menu">
                         <nav class="mdc-list mdc-drawer-submenu">
                             <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/ui-features/buttons.html">
-                                    Buttons
+                                <a href="{{ route('register') }}" class="mdc-drawer-link">
+                                    Ver
                                 </a>
                             </div>
+
                             <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/ui-features/typography.html">
-                                    Typography
+                                <a href="{{ route('register') }}" class="mdc-drawer-link">
+                                    Cadastrar
                                 </a>
                             </div>
                         </nav>
                     </div>
                 </div>
+
+                
                 <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="pages/tables/basic-tables.html">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">grid_on</i>
-                        Tables
-                    </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="pages/charts/chartjs.html">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">pie_chart_outlined</i>
-                        Charts
-                    </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel"
-                        data-target="sample-page-submenu">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">pages</i>
-                        Sample Pages
+                    <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="users-sub-menu">
+                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">people</i>
+                        Usuários
                         <i class="mdc-drawer-arrow material-icons">chevron_right</i>
                     </a>
-                    <div class="mdc-expansion-panel" id="sample-page-submenu">
+
+                    <div class="mdc-expansion-panel" id="users-sub-menu">
                         <nav class="mdc-list mdc-drawer-submenu">
                             <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/blank-page.html">
-                                    Blank Page
+                                <a href="{{ route('register') }}" class="mdc-drawer-link">
+                                    Ver
                                 </a>
                             </div>
+
                             <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/403.html">
-                                    403
-                                </a>
-                            </div>
-                            <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/404.html">
-                                    404
-                                </a>
-                            </div>
-                            <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/500.html">
-                                    500
-                                </a>
-                            </div>
-                            <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/505.html">
-                                    505
-                                </a>
-                            </div>
-                            <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/login.html">
-                                    Login
-                                </a>
-                            </div>
-                            <div class="mdc-list-item mdc-drawer-item">
-                                <a class="mdc-drawer-link" href="pages/samples/register.html">
-                                    Register
+                                <a href="{{ route('register') }}" class="mdc-drawer-link">
+                                    Cadastrar
                                 </a>
                             </div>
                         </nav>
                     </div>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link"
-                        href="https://www.bootstrapdash.com/demo/material-admin-free/jquery/documentation/documentation.html"
-                        target="_blank">
-                        <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon"
-                            aria-hidden="true">description</i>
-                        Documentation
-                    </a>
                 </div>
             </nav>
-        </div>
-        <div class="profile-actions">
-            <a href="javascript:;">Settings</a>
-            <span class="divider"></span>
-            <a href="javascript:;">Logout</a>
-        </div>
-        <div class="mdc-card premium-card">
-            <div class="d-flex align-items-center">
-                <div class="mdc-card icon-card box-shadow-0">
-                    <i class="mdi mdi-shield-outline"></i>
-                </div>
-                <div>
-                    <p class="mt-0 mb-1 ml-2 font-weight-bold tx-12">Material Dash</p>
-                    <p class="mt-0 mb-0 ml-2 tx-10">Pro available</p>
-                </div>
-            </div>
-            <p class="tx-8 mt-3 mb-1">More elements. More Pages.</p>
-            <p class="tx-8 mb-3">Starting from $25.</p>
-            <a href="https://www.bootstrapdash.com/product/material-design-admin-template/" target="_blank">
-                <span class="mdc-button mdc-button--raised mdc-button--white">Upgrade to Pro</span>
-            </a>
         </div>
     </div>
 </aside>
