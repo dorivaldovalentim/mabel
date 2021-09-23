36a51b3bca9e9fd1fe97c05b912d7a6ce017ca31
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-09-23 12:49:03 +0100

    Adicionando uma coisinha bonita na home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,23 +1,60 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__inner">
+            <div
+                class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
+                <div class="mdc-card info-card info-card--success">
+                    <div class="card-inner">
+                        <h5 class="card-title">Borrowed</h5>
+                        <h5 class="font-weight-light pb-2 mb-1 border-bottom">$62,0076.00</h5>
+                        <p class="tx-12 text-muted">48% target reached</p>
+                        <div class="card-icon-wrapper">
+                            <i class="material-icons">dvr</i>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+            <div
+                class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
+                <div class="mdc-card info-card info-card--danger">
+                    <div class="card-inner">
+                        <h5 class="card-title">Annual Profit</h5>
+                        <h5 class="font-weight-light pb-2 mb-1 border-bottom">$1,958,104.00</h5>
+                        <p class="tx-12 text-muted">55% target reached</p>
+                        <div class="card-icon-wrapper">
+                            <i class="material-icons">attach_money</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
+                <div class="mdc-card info-card info-card--primary">
+                    <div class="card-inner">
+                        <h5 class="card-title">Lead Conversion</h5>
+                        <h5 class="font-weight-light pb-2 mb-1 border-bottom">$234,769.00</h5>
+                        <p class="tx-12 text-muted">87% target reached</p>
+                        <div class="card-icon-wrapper">
+                            <i class="material-icons">trending_up</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
+                <div class="mdc-card info-card info-card--info">
+                    <div class="card-inner">
+                        <h5 class="card-title">Average Income</h5>
+                        <h5 class="font-weight-light pb-2 mb-1 border-bottom">$1,200.00</h5>
+                        <p class="tx-12 text-muted">87% target reached</p>
+                        <div class="card-icon-wrapper">
+                            <i class="material-icons">credit_card</i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
