77fb0fed7e0aeab2bdb89643ecee1c59549d4db9
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-09-27 15:12:23 +0100

    Cadastrando mais usuários via seeder para testes

--- database/seeds/GeneralSeeder.php
+++ database/seeds/GeneralSeeder.php
@@ -16,9 +16,29 @@ class GeneralSeeder extends Seeder
             'first_name' => 'Acácia',
             'last_name' => 'Valentim',
             'email' => 'acaciavalentim@gmail.com',
-            'phone' => '933802891',
+            'phone' => '990802891',
             'password' => Hash::make('qwerty'),
             'is_admin' => 0
+        ]);
+
+        DB::table('users')->insert([
+            'username' => 'dorivaldovalentim',
+            'first_name' => 'Dorivaldo',
+            'last_name' => 'Valentim',
+            'email' => 'dorivaldovalentim@gmail.com',
+            'phone' => '933802891',
+            'password' => Hash::make('qwerty'),
+            'is_admin' => 1
+        ]);
+
+        DB::table('users')->insert([
+            'username' => 'jefthvalentim',
+            'first_name' => 'Jefth',
+            'last_name' => 'Valentim',
+            'email' => 'jefthvalentim@hotmail.com',
+            'phone' => '939796801',
+            'password' => Hash::make('qwerty'),
+            'is_admin' => 2
         ]);
     }
 }
