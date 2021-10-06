7e6a1437171c7a123c9e7d32ddbda4de7398254d
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-10-07 00:21:52 +0100

    Mexendo minimamente no footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,14 +2,14 @@
     <div class="mdc-layout-grid">
         <div class="mdc-layout-grid__inner">
             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop">
-                <span class="text-center text-sm-left d-block d-sm-inline-block tx-14">Copyright © <a
-                        href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2020</span>
+                <span class="text-center text-sm-left d-block d-sm-inline-block tx-14">2021 | Copyright © <a
+                        href="https://www.dorivatech.com/" target="_blank">DorivaTech </a></span>
             </div>
             <div
                 class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop d-flex justify-content-end">
-                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center tx-14">Free <a
-                        href="https://www.bootstrapdash.com/material-design-dashboard/" target="_blank"> material admin
-                    </a> dashboards from Bootstrapdash.com</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center tx-14">Desenvolvido por <a
+                        href="https://www.bootstrapdash.com/material-design-dashboard/" target="_blank"> DorivaTech
+                    </a></span>
             </div>
         </div>
     </div>
