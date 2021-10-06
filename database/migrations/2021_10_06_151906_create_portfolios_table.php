b00115b6156d8a936bf788709648e68fb80c8b30
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-10-06 16:58:31 +0100

    Preparando a migration do portfólio

--- database/migrations/2021_10_06_151906_create_portfolios_table.php
+++ database/migrations/2021_10_06_151906_create_portfolios_table.php
@@ -15,6 +15,23 @@ class CreatePortfoliosTable extends Migration
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->on('users')->references('id');
+            $table->string('name');
+            $table->string('client')->nullable();
+            $table->text('file')->nullable();
+            $table->enum('file_type', ['image', 'video'])->nullable();
+            $table->text('description')->nullable();
+            $table->enum('type', [
+                'Vídeo Clipe',
+                'Video Lyric',
+                'Spot Publicitário',
+                'Vídeo Comercial',
+                'Vídeo de Casamento'
+            ])->nullable();
+            $table->date('begins_at')->nullable();
+            $table->date('ends_at')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
