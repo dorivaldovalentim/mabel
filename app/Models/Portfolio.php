a37c95769278fea7f0d4bf5f9e6c3f3559460839
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-10-06 18:12:19 +0100

    Preparando o fillable da model de Portfólio e criando o seu relacionamento com a model de usuário

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'client', 'description', 'file_type', 'file', 'type', 'begins_at', 'ends_at',
+    ];
+
+    /**
+     * Get the user that owns the Portfolio
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -37,4 +37,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Get all of the portfolios for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function portfolios()
+    {
+        return $this->hasMany(Models\Portfolio::class);
+    }
 }
